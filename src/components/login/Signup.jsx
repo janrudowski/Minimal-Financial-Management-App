8faef958ce7ab4b145b5af64ee5865363c49b77f
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-07-11 19:56:33 +0800

    Revert "add sign up page"
    This reverts commit 26e341c5dff14cd4a24c5c88be47469b5f4a8262.

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -1,46 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Signup.css';
 
 export default function Signup() {
-  return (
-    <div className='signup-container'>
-      <div className='signup-content'>
-        <div className='signup-form'>
-          <img
-            src={`${process.env.PUBLIC_URL}/images/logo.png`}
-            alt='maglo logo'
-            className='logo'
-          />
-          <h1>Create new account</h1>
-          <p>Welcome back! Please enter your details</p>
-          <form>
-            <label htmlFor='name'>Full Name</label>
-            <input type='text' name='name' id='name' />
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='email' />
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id='password' />
-            <button className='signup-btn'>Create Account</button>
-            <button className='signup-google-btn' type='button'>
-              <img
-                className='google-logo'
-                src={`${process.env.PUBLIC_URL}/icons/Google.png`}
-                alt=''
-              />
-              Sign up with google
-            </button>
-            <p className='have-account'>
-              Already have an account? <Link to='/signin'>Sign in</Link>
-            </p>
-          </form>
-        </div>
-      </div>
-      <img
-        className='signup-image'
-        src={`${process.env.PUBLIC_URL}/images/Image.png`}
-        alt='hand holding clock'
-      />
-    </div>
-  );
+  return <div>Signup herre</div>;
 }
