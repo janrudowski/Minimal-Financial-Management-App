61b65f4a7139f514915374e35aeb038ff75994fb
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-07-11 22:36:57 +0800

    styling changes in signin and signup page

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -31,6 +31,7 @@ export default function Signup() {
               id='password'
               placeholder='Create your password'
             />
+
             <button className='signup-btn'>Create Account</button>
             <button className='signup-google-btn' type='button'>
               <img
