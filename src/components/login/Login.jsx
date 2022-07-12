da20a11d624bdf785f182a2b3911689515bda03d
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-07-12 09:01:56 +0800

    rename css file to lowercase

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 
 export default function Login() {
   return (
--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Signup.css';
+import './signup.css';
 
 export default function Signup() {
   return (
